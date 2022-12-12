e34cd0304bb99d811d1ab38b82af45a4fa5aebde
jbaptistec <champilou.jbaptiste@gmail.com>
2022-12-12 09:40:51 -0500

    [log.sh] script 'final' avec les bon directory de destination par JBC

--- log.sh
+++ log.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 count=1  
-# n=5
-mkdir -p TEST
+n=$nb_de_creations_souhaitees
+mkdir -p /var/log/exercice/recent_logs
+# mkdir -p TEST
 while [ $count -le 5 ]
 do
 	nom_de_fichier=$(</dev/random tr -dc 'A-Z-a-z-0-9' | head -c10) 
-	echo $(</dev/random tr -dc 'A-Z-a-z-0-9' | head -c10) >> ~/EXOGIT/TEST/$nom_de_fichier
-# /var/log/exercice/recent_logs
+	echo $(</dev/random tr -dc 'A-Z-a-z-0-9' | head -c10) >> /var/log/exercice/recent_logs/$nom_de_fichier
+	# echo $(</dev/random tr -dc 'A-Z-a-z-0-9' | head -c10) >> TEST/$nom_de_fichier
 count=$((count+1))
 done
