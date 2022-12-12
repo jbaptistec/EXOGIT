cc29843d8f3b3dc649f7011e3f79d4cf77479048
jbaptistec <champilou.jbaptiste@gmail.com>
2022-12-12 09:17:52 -0500

    [log.sh] test de création d'un dossier dans lequel s'ingrémente 5 fichiers au nom aléatoire contenant une ligne de 10 caractères aléatoire par JBC

--- log.sh
+++ log.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-echo = $(</dev/random tr -dc 'A-Z-a-z-0-9' | fold -w10 | head -1) >> TEST
+count=1  
+# n=5
+mkdir -p TEST
+while [ $count -le 5 ]
+do
+	nom_de_fichier=$(</dev/random tr -dc 'A-Z-a-z-0-9' | head -c10) 
+	echo $(</dev/random tr -dc 'A-Z-a-z-0-9' | head -c10) >> ~/EXOGIT/TEST/$nom_de_fichier
+# /var/log/exercice/recent_logs
+count=$((count+1))
+done
