7929306c200e2ec5440db253f2bf4b534e7f09c5
jbaptistec <champilou.jbaptiste@gmail.com>
2022-12-13 06:52:35 -0500

    [hodor.sh] modification du script pour créer un nouveau fichier d'archivage à chaque activation de hodor.sh et utilisation du paramètre d'entrée par JBC

--- hodor.sh
+++ hodor.sh
@@ -1,34 +1,28 @@
 #!/bin/sh
 # récupération du % d'inode utilisées
 inode_used=$(df -i ~/EXOGIT/ |grep -v Filesystem|awk '{print $5}'|sed 's/\%//')
+
+#initialisation de la limite
+n=$limit_stock
+
 # echo $(df -i ~/EXOGIT/ |grep -v Filesystem|awk '{print $5}'|sed 's/\%//')
 # echo $inode_used
 
-# création fichier archivage
-# vérification préalable : vérification de l'existence d'un dossier d'archivage
-if [ ! -d "/var/log/exercice/archives"]; then
+# conditions pour lancer l'action
+if $inode_used -ge n; then
 	# création du directory
 	mkdir -p /var/log/exercice/archives/
 	# création du nom du fichier archivage
 	fichier_archive=$(mktemp -u XXXXXXXXXXX)
 	# création du fichier archivage
 	touch /var/log/exercice/archives/$fichier_archive
-	#création d'un alias permanent définissant le nom unique du fichier archivage
-	echo 'alias archi="$fichier_archive"' >> ~/.zshrc
-	# prise en compte alias après ajout
-	source ~/.zshrc
-fi
-
-
-# conditions pour lancer l'action
-if $inode_used -ge 20; then
 	# compression et archivage dans le fichier archivage créé
 	tar -czvf /var/log/exercice/archives/archi /var/log/exercice/recent_logs	# affichage du message sur tous les terminaux ouvert sur la machine
 	wall "HODOOOR !!!"
 fi
 
 # arrêt de start si malgrès l'archivage et compression la quantité d'inode utilisé est toujours trop importante
-if $inode_used -ge 20; then
+if $inode_used -ge n; then
 	kill $(pgrep start.sh)
 	echo "Plus d'inode disponible, arrêt de start.sh"
 	crontab -r
