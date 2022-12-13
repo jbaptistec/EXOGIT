0c8857c8903469974d7654c7dbee557de7661e22
jbaptistec <champilou.jbaptiste@gmail.com>
2022-12-12 19:43:00 -0500

    [hodor.sh] écriture du script hodor.sh non testé par JBC

--- hodor.sh
+++ hodor.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 # récupération du % d'inode utilisées
-inode_\%=$(df -i ~/EXOGIT/ |grep -v Filesystem|awk '{print $5}'|sed 's/\%//')
+inode_used=$(df -i ~/EXOGIT/ |grep -v Filesystem|awk '{print $5}'|sed 's/\%//')
 # echo $(df -i ~/EXOGIT/ |grep -v Filesystem|awk '{print $5}'|sed 's/\%//')
-# conditions pour lancer l'action
-if $inode_\% -ge 80
+# echo $inode_used
 
+# création fichier archivage
+# vérification préalable 1: vérification de l'existance d'un dossier d'archivage
+if [ ! -d "/var/log/exercice/archives"]; then
+	# création du directory
+	mkdir -p /var/log/exercice/archives/
+	# création du nom du fichier archivage
+	fichier_archive=$(mktemp -u XXXXXXXXXXX)
+	# création du fichier archivage
+	touch /var/log/exercice/archives/$fichier_archive
+	#création d'un alias permanent définissant le nom unique du fichier archivage
+	echo 'alias archi="$fichier_archive"' >> ~/.zshrc
+	# prise en compte alias après ajout
+	source ~/.zshrc
+fi
+
+
+# conditions pour lancer l'action
+if $inode_used -ge 20; then
+	# compression et archivage dans le fichier archivage créé
+	tar -czvf /var/log/exercice/archives/archi /var/log/exercice/recent_logs	# affichage du message sur tous les terminaux ouvert sur la machine
+	wall "HODOOOR !!!"
+fi
+
+# if $inode_used -ge 20; then
+# STOP start.sh
+ 
+ 
