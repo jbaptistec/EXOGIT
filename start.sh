e13aa77ec02d2d1e49a7ac5666aa6e7d0f06eb6b
jbaptistec <champilou.jbaptiste@gmail.com>
2022-12-13 07:10:50 -0500

    [start.sh] correction erreur synthaxe dans boucle while par JBC

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "Combien souhaitez-vous créer de fichier par execution de log.sh"
 read nb_voulu
 
 #vérification que c'est un nombre non null ou = 0
-while $nb_voulu=NULL || $nb_voulu=0
+while $nb_voulu -eq NULL || $nb_voulu -eq 0
 do
 	echo "nb_voulu ne peut-être NULL ou égal à 0"
 	echo "Combien souhaitez-vous créer de fichier par execution de log.sh"
@@ -17,7 +17,7 @@ echo "Quelle limite de stockage souhaitez-vous fixer"
 read limit_stock
 
 #vérification que c'est un nombre non null ou = 0
-while $limit_stock=NULL || $limit_stock=0
+while $limit_stock -eq NULL || $limit_stock -eq 0
 do
         echo "limit_stock ne peut-être NULL ou égal à 0"
         echo "Combien souhaitez-vous créer de fichier par execution de log.sh"
