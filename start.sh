b76fc938b3a405566196f110b3afce73f32d76fc
jbaptistec <champilou.jbaptiste@gmail.com>
2022-12-13 08:31:47 -0500

    [start.sh] modifications du script par JBC

--- start.sh
+++ start.sh
@@ -25,9 +25,9 @@ do
 	read limit_stock
 done
 
-line="* * * * * sudo bash /home/jbaptistec/EXOGIT/log.sh $nb_voulu"
-(sudo crontab -u root -l; echo "$line") | crontab -u root
+line="* * * * * sudo sh /home/jbaptistec/EXOGIT/log.sh $nb_voulu"
+(crontab -u root -l; echo "$line") | crontab -u root
 
-line="*/5 * * * * sudo bash /home/jbaptistec/EXOGIT/hodor.sh $limit_stock"
-(sudo crontab -u root -l; echo "$line") | crontab -u root
+line="*/5 * * * * sudo sh /home/jbaptistec/EXOGIT/hodor.sh $limit_stock"
+(crontab -u root -l; echo "$line") | crontab -u root
 
