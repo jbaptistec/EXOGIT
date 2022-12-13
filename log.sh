c7d5cf8407977c9a442733bf7a80856dc5c0fbc5
jbaptistec <champilou.jbaptiste@gmail.com>
2022-12-13 03:44:23 -0500

    [log.sh] définition du nombre de répétition de la boucle n=1000 par JBC

--- log.sh
+++ log.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 count=1  
-n=$nb_de_creations_souhaitees
+n=1000
 mkdir -p /var/log/exercice/recent_logs
 # mkdir -p TEST
-while [ $count -le 5 ]
+while [ $count -le $n ]
 do
 	nom_de_fichier=$(</dev/random tr -dc 'A-Z-a-z-0-9' | head -c10) 
 	echo $(</dev/random tr -dc 'A-Z-a-z-0-9' | head -c10) >> /var/log/exercice/recent_logs/$nom_de_fichier
